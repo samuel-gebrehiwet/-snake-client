81205ee47d63be3ecc81e2f0cff99bbb197e14c9
Samuel Gebrehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-08-20 06:04:53 +0000

    Implement snake movement controls and send movement commands to the server

--- input.js
+++ input.js
@@ -1,18 +1,30 @@
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+// input.js
 
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
   stdin.on("data", handleUserInput);
+};
 
-  return stdin;
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
+  }
 };
 
 module.exports = { setupInput };
