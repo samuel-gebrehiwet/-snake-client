bc220f53bead43c0c8639dbc988f4d93df5ef544
Samuel Gebrehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-08-20 05:12:21 +0000

    seperated constant variables

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-// Establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost', // Replace with the server's IP address
-    port: 50541, // Replace with the server's port number
-  });
-
-  // Interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // Event listener for incoming data
-  conn.on("data", (data) => {
-    console.log("Message from server:", data);
-  });
-
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
