a58a42324c53272681023fade1cd40dbb155d930
Samuel Gebrehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-08-20 05:45:33 +0000

    snake start moveing by setTimeout

--- play.js
+++ play.js
@@ -1,5 +1,24 @@
-const { connect } = require('./client');
+const net = require("net");
+const { connect } = require("./client"); // Import the connect function from client.js
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput); // Register the event listener for keyboard input
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') { // Check for CTRL + C input (End of Text)
+    process.exit(); // Terminate the game
+  }
+};
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
+setupInput(conn);
