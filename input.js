ae942485e77495d0afc8bfad9a3cf0b335ea25f0
Samuel Gebrehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-08-20 06:08:32 +0000

    Implement 'Say' message feature with canned messages

--- input.js
+++ input.js
@@ -12,19 +12,22 @@ const setupInput = (conn) => {
 
   stdin.on("data", handleUserInput);
 };
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  } else if (key === 'w') {
+const handleUserInput = function (key) {
+  if (key === "w") {
     connection.write("Move: up");
-  } else if (key === 'a') {
+  } else if (key === "a") {
     connection.write("Move: left");
-  } else if (key === 's') {
+  } else if (key === "s") {
     connection.write("Move: down");
-  } else if (key === 'd') {
+  } else if (key === "d") {
     connection.write("Move: right");
-  }
+  } else if (key === "1") {
+    connection.write("Say: Hello!");
+  } else if (key === "2") {
+    connection.write("Say: Have fun!");
+  } else if (key === "3") {
+    connection.write("Say: LOL");
+  } // Add more cases for other keys/messages
 };
 
 module.exports = { setupInput };
