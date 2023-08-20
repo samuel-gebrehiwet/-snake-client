5bff328640c09cac5f1256b0edc4ff33637d31e1
Samuel Gebrehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-08-20 05:59:09 +0000

    refactore

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
-const net = require("net");
-const { connect } = require("./client"); // Import the connect function from client.js
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput); // Register the event listener for keyboard input
-
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') { // Check for CTRL + C input (End of Text)
-    process.exit(); // Terminate the game
-  }
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 const conn = connect();
